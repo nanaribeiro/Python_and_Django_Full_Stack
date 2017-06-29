5c1adbd2d2e9433a70ad72f70f4c64bf5ec9a366
Alana Ribeiro Ramos Sasse <alana.sasse@globaltec.com.br>
2017-06-29 13:06:55 -0300

    Added semi-colon because it is a good practice in JavaScript!

--- myFirstJS.js
+++ myFirstJS.js
@@ -1,4 +1,4 @@
-var weight = prompt("Enter your weight in pounds (lbs)")
-var conversion = weight*0.454
-alert("Your weight in quilograms is " + conversion + "Kg")
+var weight = prompt("Enter your weight in pounds (lbs)");
+var conversion = weight*0.454;
+alert("Your weight in quilograms is " + conversion + "Kg");
 console.log("Conversion completed");
