958a334b578bac9eb34b95a9e5ba9ca54c073f4d
Alana Ribeiro Ramos Sasse <alana.sasse@globaltec.com.br>
2017-06-29 13:03:24 -0300

    Linked html file to JavaScript and made mmy first JavaScriptgit add .

---
+++ myFirstJS.js
@@ -0,0 +1,4 @@
+var weight = prompt("Enter your weight in pounds (lbs)")
+var conversion = weight*0.454
+alert("Your weight in quilograms is " + conversion + "Kg")
+console.log("Conversion completed");
